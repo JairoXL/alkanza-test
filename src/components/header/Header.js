1bf6480e318a9ebb1e051ca5352c3fe8f5d18ae2
Jairo Andrés Mendieta Reyes <jairo@fanxielab.com>
2019-02-09 22:22:24 -0500

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from '../../assets/img/logo.png';
+import './header.scss';
+
+class Header extends Component {
+    render(){
+        return(
+            <header className="header" >
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                    <div className="container">
+                        <div className="navbar-start">
+                            <div className="navbar-brand">
+                                <a className="navbar-item" href="https://www.alkanza.us">
+                                    <img src={logo} width="112" height="28" alt={'Alkanza'}/>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="navbar-end">
+                            <div className="navbar-item">
+                                <h3>FS TEST</h3>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default Header;
---
+++ src/controller/Calcs.js
@@ -0,0 +1,20 @@
+export default {
+
+    getDistancePoints(lat1,lon1,lat2,lon2) {
+        let R = 6371;
+        let dLat = this.degRad(lat2-lat1);  // deg2rad below
+        let dLon = this.degRad(lon2-lon1);
+        let a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(this.degRad(lat1)) * Math.cos(this.degRad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        let d = R * c; // Distance in km
+        return d;
+    },
+
+    degRad(deg) {
+        return deg * (Math.PI/180)
+    }
+
+}
